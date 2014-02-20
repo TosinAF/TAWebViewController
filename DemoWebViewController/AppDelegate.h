ebc72c75ad94b26bd19950542481f49e56c6b502
Tosin Afolabi <psyooa@nottingham.ac.uk>
2014-02-20 01:19:51 +0000

    Initial Commit

---
+++ DemoWebViewController/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DemoWebViewController
+//
+//  Created by Tosin Afolabi on 20/02/2014.
+//  Copyright (c) 2014 Tosin Afolabi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DemoWebViewControllerTests/DemoWebViewControllerTests.m
@@ -0,0 +1,34 @@
+//
+//  DemoWebViewControllerTests.m
+//  DemoWebViewControllerTests
+//
+//  Created by Tosin Afolabi on 20/02/2014.
+//  Copyright (c) 2014 Tosin Afolabi. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DemoWebViewControllerTests : XCTestCase
+
+@end
+
+@implementation DemoWebViewControllerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
